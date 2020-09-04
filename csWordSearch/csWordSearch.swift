1e4f1a314a43b886ab3c19604f1570a56ac05066
Anshpreet <anshpreet5678@github.com>
2020-09-04 09:50:47 -0400

    started displaying the grid

--- csWordSearch/csWordSearch.swift
+++ csWordSearch/csWordSearch.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-// indicates whether the word will be horizontal, vertical, diagonal, etc
+// this enum indicates whether the word will be horizontal, vertical, diagonal, etc
 enum WordPlacement: CaseIterable {
     case downToUp
     case upToDown
@@ -18,4 +18,65 @@ enum WordPlacement: CaseIterable {
     case topLeftToBottomRight
     case bottomRightToTopLeft
     case bottomLeftToTopRight
+    
+    //indicates where on the grid the user moves, depending on how the word is placed
+    var axes: (x: Int, y: Int) {
+        switch self {
+        case .downToUp: return (0, -1)
+        case .upToDown: return (0, 1)
+        case .leftToRight: return (1, 0)
+        case .rightToLeft: return (-1, 0)
+        case .topRightToBottomLeft: return (-1, 1)
+        case .topLeftToBottomRight: return (1, 1)
+        case .bottomRightToTopLeft: return (-1, -1)
+        case .bottomLeftToTopRight: return (1, -1)
+        }
+    }
 }
+
+// return WordPlacement.allCases.shuffled()
+
+// what the word is
+struct Word: Decodable {
+    var word: String
+}
+
+// MARK: WordSearch Class
+
+class Label {
+    var letter: Character = " "
+}
+
+class csWordSearch {
+    // words will be an array of the Word struct
+    var words = [Word]()
+    var gridSize = 10
+    var labels = [[Label]]()
+    
+    // wrap the lowercase letters into an array
+    let fillInLetters = (97...122).map {
+        Character(Unicode.Scalar($0))
+    }
+    
+    // create the grid
+    func createGrid() {
+        // labl will be an array of arrays of Label
+        labels = (0 ..< 10).map { _ in
+            (0 ..< 10).map { _ in Label() }
+        }
+        
+        fillInGaps()
+        displayGrid()
+    }
+    
+    private func fillInGaps() {
+        for column in labels {
+            for label in column {
+                if label.letter == " " {
+                    label.letter = fillInLetters.randomElement()!
+                }
+            }
+        }
+    }
+}
+
